e1d49a1b40f396ec768e71e5614d1454fc9c1d94
Zoltan Vegh <vegh.z@aol.com>
2020-11-17 15:39:52 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -3,10 +3,10 @@ const phrase = document.querySelector("#phrase");
 let missed = 0;
 const phrases = [
   "Ghost of Tsushima",
-  "Cold War",
+  "I am learning javascript",
   "Pacific Ocean",
   "Madagascar",
-  "Ireland",
+  "My next destination is Ireland",
 ];
 
 const overlay = document.querySelector("#overlay");
@@ -17,13 +17,21 @@ startButton.addEventListener("click", () => {
 
 const dieRoll = Math.floor(Math.random() * 5); // Random number between 0-5
 
-function getRandomPhraseAsArray(arr) {
+// return a random phrase from the array
+const getRandomPhraseAsArray = (arr) => {
   let randomPhrase = arr[dieRoll];
   for (let i = 0; i < randomPhrase.length; i++) {
     console.log(randomPhrase.charAt(i));
+    let li = document.createElement("li");
+    li.textContent = arr[dieRoll].value;
+    phrase.appendChild(li);
+    if (arr[i] !== " ") {
+      li.className = "letter";
+    } else {
+      li.className = "";
+    }
   }
-  console.log(arr[dieRoll]);
-  return randomPhrase;
-}
+  return phrases[dieRoll];
+};
 
 getRandomPhraseAsArray(phrases);
